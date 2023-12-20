4e1f7101bae42585c03da794188f3ec9ba1c4f88
sabithaaulia <sabithaaulia@student.telkomuniversity.ac.id>
2023-12-20 22:10:02 +0700

    Init all

---
+++ app/Http/Controllers/Api/MenuController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Menu::orderBy('nama_menu','asc')->get();
+        return response()->json([
+            'status'=>true,
+            'message'=>'data ditemukan',
+            'data'=>$data
+        ],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
