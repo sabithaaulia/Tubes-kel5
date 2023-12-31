223f10843fd01a7a7b89f72890fc13eaae5f823c
IDewaAgungRama <idewaagungrama@student.telkomuniversity.ac.id>
2023-12-31 20:52:19 +0700

    kontrak tenant fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\TenantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,10 @@ Route::get('/products/edit', function () {
     return view('edit');
 });
 
+Route::get('/tenant-contract', function () {
+    return view('tenant-contract');
+});
+
 Route::get('/debug-cart', function () {
     dd(session('cart'));
 });
@@ -42,3 +47,9 @@ Route::delete('/cart/remove/{productId}', [CartController::class, 'removeFromCar
 Route::put('/cart/update/{productId}', [CartController::class, 'updateCart']);
 Route::get('/cart', [CartController::class, 'showCart']);
 Route::delete('cart/remove/{productId}', [CartController::class, 'removeFromCart']);
+
+// Define route for deleting tenant contracts
+Route::delete('/tenant-contract/{id}', [TenantController::class, 'destroy'])->name('tenant-contract.destroy');
+Route::get('/tenant-contract', [TenantController::class, 'index']);
+
+Route::resource('tenant-contract', TenantController::class);
